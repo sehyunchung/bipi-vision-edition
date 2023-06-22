b1676b60f2786ce39cded97628f21491afb78abd
Sehyun Chung <sehyun.chung@icloud.com>
2023-06-22 20:32:48 +0900

    Initial Commit

---
+++ Bipi! Vision Edition/Bipi__Vision_EditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bipi__Vision_EditionApp.swift
+//  Bipi! Vision Edition
+//
+//  Created by Sehyun Chung on 2023/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Bipi__Vision_EditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
